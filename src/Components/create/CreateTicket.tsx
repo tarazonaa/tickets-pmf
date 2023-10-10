aaafa995a3ab690567277faa4522ec979cc64502
ronuma <ronum.00@gmail.com>
2023-10-09 18:13:33 -0600

    changed closing comment to resolution process in labels

--- src/Components/create/CreateTicket.tsx
+++ src/Components/create/CreateTicket.tsx
@@ -1,121 +1,105 @@
-import {
-  Create,
-  SimpleForm,
-  TextInput,
-  required,
-  SelectInput,
-  useGetList,
-} from "react-admin";
-import { useState } from "react";
+import {Create, SimpleForm, TextInput, required, SelectInput, useGetList} from "react-admin"
+import {useState} from "react"
 
 export const CreateTicket = () => {
-  const [selectedCategory, setSelectedCategory] = useState("");
-  const { data, isLoading } = useGetList("categories");
+   const [selectedCategory, setSelectedCategory] = useState("")
+   const {data, isLoading} = useGetList("categories")
 
-  const validateDescription = (value: string) => {
-    if (value && value.length > 500) {
-      return "Descripción debe de ser máximo 500 caracteres";
-    }
-    return undefined;
-  };
-  const validateClosing = (value: string) => {
-    if (value && value.length > 250) {
-      4;
-      return "Comentario de cierre debe de ser máximo 250 caracteres";
-    }
-    return undefined;
-  };
-
-  const subcategories: {
-    index: number;
-    name: string;
-    parentCategoryId: string;
-  }[] = [];
-  if (data) {
-    data.forEach(
-      (category: { id: string; name: string; subcategories: string[] }) => {
-        category?.subcategories.forEach(
-          (subcategory: string, index: number) => {
-            subcategories.push({
-              parentCategoryId: category.id,
-              index: index,
-              name: subcategory,
-            });
-          }
-        );
+   const validateDescription = (value: string) => {
+      if (value && value.length > 500) {
+         return "Descripción debe de ser máximo 500 caracteres"
       }
-    );
-  }
-  return (
-    <Create>
-      <SimpleForm>
-        <TextInput source="title" validate={[required()]} label="Título" />
-        <SelectInput
-          source="categoryId"
-          label="Categoría"
-          choices={data}
-          optionText="name"
-          optionValue="id"
-          validate={[required()]}
-          isLoading={isLoading}
-          onChange={(e:any) => setSelectedCategory(e.target.value)}
-        />
+      return undefined
+   }
+   const validateClosing = (value: string) => {
+      if (value && value.length > 250) {
+         4
+         return "Comentario de cierre debe de ser máximo 250 caracteres"
+      }
+      return undefined
+   }
 
-        <SelectInput
-          source="subcategoryIndex"
-          label="Subcategoria"
-          choices={subcategories.map((subcategory) => {
-            if (subcategory.parentCategoryId === selectedCategory) {
-              return subcategory;
-            }
-            return null;
-          })}
-          optionText="name"
-          optionValue="index"
-          validate={[required()]}
-          isLoading={isLoading}
-        />
+   const subcategories: {
+      index: number
+      name: string
+      parentCategoryId: string
+   }[] = []
+   if (data) {
+      data.forEach((category: {id: string; name: string; subcategories: string[]}) => {
+         category?.subcategories.forEach((subcategory: string, index: number) => {
+            subcategories.push({
+               parentCategoryId: category.id,
+               index: index,
+               name: subcategory,
+            })
+         })
+      })
+   }
+   return (
+      <Create>
+         <SimpleForm>
+            <TextInput source="title" validate={[required()]} label="Título" />
+            <SelectInput
+               source="categoryId"
+               label="Categoría"
+               choices={data}
+               optionText="name"
+               optionValue="id"
+               validate={[required()]}
+               isLoading={isLoading}
+               onChange={(e: any) => setSelectedCategory(e.target.value)}
+            />
 
-        <SelectInput
-          validate={[required()]}
-          source="priority"
-          label="Prioridad"
-          choices={[
-            { id: 0, name: "Baja" },
-            { id: 1, name: "Media" },
-            { id: 2, name: "Alta" },
-          ]}
-        />
-        <SelectInput
-          validate={[required()]}
-          source="status"
-          label="Estatus"
-          choices={[
-            { id: 0, name: "Pendiente" },
-            { id: 1, name: "En progreso" },
-            { id: 2, name: "Cerrado" },
-          ]}
-        />
-        <TextInput
-          source="description"
-          multiline={true}
-          label="Descripción"
-          sx={{ width: "50%" }}
-          validate={validateDescription}
-        />
-        <TextInput
-          source="closingComment"
-          multiline={true}
-          label="Comentario de cierre"
-          validate={validateClosing}
-          sx={{ width: "50%" }}
-        />
-      </SimpleForm>
-    </Create>
-  );
-};
+            <SelectInput
+               source="subcategoryIndex"
+               label="Subcategoria"
+               choices={subcategories.map((subcategory) => {
+                  if (subcategory.parentCategoryId === selectedCategory) {
+                     return subcategory
+                  }
+                  return null
+               })}
+               optionText="name"
+               optionValue="index"
+               validate={[required()]}
+               isLoading={isLoading}
+            />
 
-// Need to fetch /categories has subcategories. So it is a dropdowm with subcategories.
-// Needd to fetch from /categories the index of subcategories.
-// NOT YET IMPLEMENTED:
-// source="classroomId"
+            <SelectInput
+               validate={[required()]}
+               source="priority"
+               label="Prioridad"
+               choices={[
+                  {id: 0, name: "Baja"},
+                  {id: 1, name: "Media"},
+                  {id: 2, name: "Alta"},
+               ]}
+            />
+            <SelectInput
+               validate={[required()]}
+               source="status"
+               label="Estatus"
+               choices={[
+                  {id: 0, name: "Pendiente"},
+                  {id: 1, name: "En progreso"},
+                  {id: 2, name: "Cerrado"},
+               ]}
+            />
+            <TextInput
+               source="description"
+               multiline={true}
+               label="Descripción"
+               sx={{width: "50%"}}
+               validate={validateDescription}
+            />
+            <TextInput
+               source="closingComment"
+               multiline={true}
+               label="Proceso de resolución"
+               validate={validateClosing}
+               sx={{width: "50%"}}
+            />
+         </SimpleForm>
+      </Create>
+   )
+}
